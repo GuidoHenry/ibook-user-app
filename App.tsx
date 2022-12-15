893daf0c7a8f9a50683222783e9d5635cdcca01d
Alexis Ariel Andermatten <alexis.andermatten@corp.smtsoftware.com>
2022-12-15 16:53:10 -0300

    [https://trello.com/c/jkMHtYFn/51-crear-estructura-de-proyecto-app] - se crea la arquitectura principal de la app-mobile

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Proyecto Mobile</Text>
+    </View>
+  );
+};
+
+export default App;
